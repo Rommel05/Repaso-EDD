7ad89a40716ea51b2b4076dffc86da6372244c2d
Rommel05 <rrll20220@gmail.com>
2024-02-20 20:21:33 +0100

    EDD

--- src/refactorizar/Fecha.java
+++ src/refactorizar/Fecha.java
@@ -1,14 +1,32 @@
 package refactorizar;
 
+/**
+ * Clase fecha:
+ * Comprueba si una fecha es valida
+ * @author Rommel
+ * @version 1.0, 20/02/2024
+ */
 class Fecha {
     private int dia;
     private int mes;
     private int anio;
+
+    /**
+     *Constructor de la clase fecha
+     * @param dia Variable de tipo entero que corresponde al dia
+     * @param mes Variable de tipo entero que corresponde al mes
+     * @param anio Variable de tipo entero que corresponde al anio
+     */
     public Fecha(int dia, int mes, int anio) {
         this.dia = dia;
         this.mes = mes;
         this.anio = anio;
     }
+
+    /**
+     * Clase para ver si la fecha es válida
+     * @return regresa si la fecha es valido o no
+     */
     public boolean valida() {
         if (dia < 1 || dia > 31) return false;
         if (mes < 1 || mes > 12) return false;
@@ -21,6 +39,10 @@ class Fecha {
             return true;
     }
 
+    /**
+     * Devuelve el dia máximo del mes
+     * @return devuelve diasMes que calcula el dia máximo del mes
+     */
     private int diaMes() {
         int diasMes = 0;
         switch (mes) {
@@ -44,6 +66,10 @@ class Fecha {
         return diasMes;
     }
 
+    /**
+     * Comprueba si el año es bisiesto
+     * @return regresa diasMes 29 si es bisiesto o 28 si no lo es
+     */
     private int añoBisiesto() {
         int diasMes;
         if ((anio % 400 == 0) || ((anio % 4 == 0) && (anio %
